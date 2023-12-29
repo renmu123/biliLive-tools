906a88478d01b8e75c27f2e581e2abe6c04a9a2a
muse <fyx@030buy.jp>
2023-12-29 23:33:25 +0800

    依赖更新

--- src/main/index.ts
+++ src/main/index.ts
@@ -83,7 +83,7 @@ function createWindow(): void {
     webPreferences: {
       preload: join(__dirname, "../preload/index.js"),
       sandbox: false,
-      webSecurity: false,
+      webSecurity: true,
     },
   });
 
