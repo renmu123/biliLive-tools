348f02401cc30d30b31f9b0e9122b43d38dbc66c
muse <fyx@030buy.jp>
2024-05-20 16:27:33 +0800

    修复大小写

--- packages/app/src/renderer/src/App.vue
+++ packages/app/src/renderer/src/App.vue
@@ -51,7 +51,7 @@ import {
   GitPullRequestOutline as QueueIcon,
 } from "@vicons/ionicons5";
 import defaultUserAvatar from "./assets/images/moehime.jpg";
-import AppSettingDialog from "./pages/Setting/index.vue";
+import AppSettingDialog from "./pages/setting/index.vue";
 import { useUserInfoStore, useQueueStore, useAppConfig } from "./stores";
 
 const quenuStore = useQueueStore();
