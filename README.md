f623c19f854729f5f49d025c15b1ff69d6eff204
renmu123 <1101022351@qq.com>
2024-08-04 15:56:55 +0800

    update for ci

--- README.md
+++ README.md
@@ -237,6 +237,10 @@ $ pnpm install
 $ pnpm run install:bin
 ```
 
+`弹幕分析`功能依赖于sqlite3，如果你无法编译安装，尝试安装[visual-studio2022](https://visualstudio.microsoft.com/zh-hans/thank-you-downloading-visual-studio/?sku=Community)，以及python3(也许)。  
+如果你是Win，你还可能遇到报错比如`cause=fork/exec %1 is not a valid Win32 application.`，根据[提示](https://github.com/pnpm/pnpm/issues/5638)修改pnpm源文件。  
+如果你最后还是无法安装，且开发的内容不包含`弹幕分析`功能，请尝试注释掉`sqlite3`依赖，打包可以依赖于`github-action`进行。
+
 如果二进制依赖安装失败或者不支持你的平台，请尝试[手动下载安装](https://github.com/renmu123/biliLive-tools/releases/tag/0.2.1)二进制依赖。
 
 新建`packages\app\resources\bin`文件夹，里面需要三个个文件。
---
+++ scripts/github-ci-pnpm-update.js
@@ -0,0 +1,49 @@
+import fs from "fs";
+import { exec } from "child_process";
+
+async function getPnpmVersion() {
+  return new Promise((resolve, reject) => {
+    exec("pnpm --version", (error, stdout, stderr) => {
+      if (error) {
+        console.error(`执行错误: ${error}`);
+        reject(error);
+        return;
+      }
+      if (stderr) {
+        console.error(`标准错误: ${stderr}`);
+        reject(stderr);
+        return;
+      }
+      const pnpmVersion = stdout.trim();
+      console.log(`当前 pnpm 版本: ${pnpmVersion}`);
+      resolve(pnpmVersion);
+    });
+  });
+}
+
+// https://github.com/pnpm/pnpm/issues/5638
+async function updatePnpm() {
+  if (process.platform !== "win32") return;
+
+  const version = await getPnpmVersion();
+  // C:\Users\runneradmin\setup-pnpm\node_modules\.pnpm\pnpm@9.6.0\node_modules\pnpm\bin\pnpm.cjs
+  // github的runner环境中，pnpm安装在C:\Users\runneradmin\setup-pnpm\node_modules\.pnpm
+  const filePath = `C:\\Users\\runneradmin\\setup-pnpm\\node_modules\\.pnpm\\pnpm@${version}\\node_modules\\pnpm\\bin\\pnpm.cjs`;
+  // 修改第一行为 #!node
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    const result = data.replace(/^#!.*\n/, "#!node\n");
+    fs.writeFile(filePath, result, "utf8", (err) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      console.log("修改 pnpm.cjs 成功", result);
+    });
+  });
+}
+
+updatePnpm();
