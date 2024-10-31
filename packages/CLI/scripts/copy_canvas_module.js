d63baa81d82e939f1e90e9d596270f93dc586ebb
renmu123 <1101022351@qq.com>
2024-10-31 10:45:22 +0800

    完善pkg打包

---
+++ packages/CLI/scripts/copy_canvas_module.js
@@ -0,0 +1,22 @@
+import fs from "node:fs";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const cli_node_modules = path.resolve(__dirname, "../node_modules");
+const pnpm_node_modules = path.resolve(__dirname, "../../../node_modules");
+
+// console.log("__dirname", __dirname, pnpm_node_modules);
+
+function main() {
+  // 找到@napi-rs相关包，复制到cli_node_modules,这个路径可能不存在，不存在则创建
+  if (!fs.existsSync(cli_node_modules)) {
+    fs.mkdirSync(cli_node_modules);
+  }
+  // 复制文件
+  fs.cpSync(path.join(pnpm_node_modules, "@napi-rs"), path.join(cli_node_modules, "@napi-rs"), {
+    recursive: true,
+  });
+}
+
+main();
