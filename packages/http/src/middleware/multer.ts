c623e1575f3818a2349b6bb774f18cfcc61a173c
renmu123 <1101022351@qq.com>
2024-11-13 17:24:15 +0800

    更新types

--- packages/http/src/middleware/multer.ts
+++ packages/http/src/middleware/multer.ts
@@ -13,6 +13,23 @@
 
 import originalMulter from "multer";
 
+declare module "koa" {
+  interface Request {
+    body?: any;
+    rawBody: string;
+    file?: {
+      fieldname: string;
+      originalname: string;
+      encoding: string;
+      mimetype: string;
+      destination: string;
+      filename: string;
+      path: string;
+      size: number;
+    };
+  }
+}
+
 function multer(options) {
   const m = originalMulter(options);
 
