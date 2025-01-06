62f45e62bb19700fef1bc980053db65323d2975a
renmu123 <1101022351@qq.com>
2025-01-06 18:11:54 +0800

    更多提示

--- packages/app/src/renderer/src/pages/Tools/pages/Recorder/components/addModal.vue
+++ packages/app/src/renderer/src/pages/Tools/pages/Recorder/components/addModal.vue
@@ -10,6 +10,7 @@
     >
       <n-form label-placement="left" :label-width="140">
         <h4>支持斗鱼、虎牙平台、B站，这并非是经过严格检测的录播工具，请谨慎在生产环境使用</h4>
+        <h4>如果想转封装为mp4，请使用webhook或者转码功能</h4>
 
         <n-form-item v-if="!isEdit">
           <template #label>
