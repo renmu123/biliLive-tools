589d1e4e0b57d6280de057edd45ad14df79ef6fd
renmu123 <1101022351@qq.com>
2025-01-22 21:51:53 +0800

    为B站预设新增封面文件大小校验

--- packages/app/src/renderer/src/components/ImageCrop.vue
+++ packages/app/src/renderer/src/components/ImageCrop.vue
@@ -16,7 +16,22 @@
     >
       <img v-if="src" class="image" :src="src" @click="selectImage" />
       <div v-else class="empty-image" @click="selectImage">选择图片</div>
-      <span v-if="src" class="remove" @click="remove">X</span>
+      <n-icon v-if="src" size="14" class="remove" @click="remove">
+        <svg
+          viewBox="0 0 12 12"
+          version="1.1"
+          xmlns="http://www.w3.org/2000/svg"
+          aria-hidden="true"
+        >
+          <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+            <g fill="currentColor" fill-rule="nonzero">
+              <path
+                d="M2.08859116,2.2156945 L2.14644661,2.14644661 C2.32001296,1.97288026 2.58943736,1.95359511 2.7843055,2.08859116 L2.85355339,2.14644661 L6,5.293 L9.14644661,2.14644661 C9.34170876,1.95118446 9.65829124,1.95118446 9.85355339,2.14644661 C10.0488155,2.34170876 10.0488155,2.65829124 9.85355339,2.85355339 L6.707,6 L9.85355339,9.14644661 C10.0271197,9.32001296 10.0464049,9.58943736 9.91140884,9.7843055 L9.85355339,9.85355339 C9.67998704,10.0271197 9.41056264,10.0464049 9.2156945,9.91140884 L9.14644661,9.85355339 L6,6.707 L2.85355339,9.85355339 C2.65829124,10.0488155 2.34170876,10.0488155 2.14644661,9.85355339 C1.95118446,9.65829124 1.95118446,9.34170876 2.14644661,9.14644661 L5.293,6 L2.14644661,2.85355339 C1.97288026,2.67998704 1.95359511,2.41056264 2.08859116,2.2156945 L2.14644661,2.14644661 L2.08859116,2.2156945 Z"
+              ></path>
+            </g>
+          </g>
+        </svg>
+      </n-icon>
     </div>
   </div>
 </template>
@@ -45,9 +60,17 @@ const props: Props = withDefaults(defineProps<Props>(), {
   width: "160px",
 });
 
+const notice = useNotification();
 const handleCoverChange = async (e: Event) => {
   const file = (e.target as HTMLInputElement).files?.[0];
   if (!file) return;
+  console.log(file);
+  if (file.size > 1024 * 1024 * 2) {
+    notice.warning({
+      title: "图片大小超过2M可能导致无法上传成功~",
+      duration: 2000,
+    });
+  }
   const res = await commonApi.uploadCover(file);
   filename.value = res.name;
 };
@@ -87,8 +110,8 @@ const remove = () => {
   position: absolute;
   top: 4px;
   right: 4px;
-  color: #fff;
-  background: #000000;
+  color: rgba(0, 0, 0, 0.8);
+  background: #f0f0f0;
   border-radius: 50%;
   cursor: pointer;
   font-size: 12px;
