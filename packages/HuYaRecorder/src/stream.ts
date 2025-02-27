2648244ccb95b0fd11a9bfbbf4dc70466dfd4383
renmu123 <1101022351@qq.com>
2025-02-27 16:55:01 +0800

    `quality` 参数支持的值修改

--- packages/HuYaRecorder/src/stream.ts
+++ packages/HuYaRecorder/src/stream.ts
@@ -1,5 +1,5 @@
 import { sortBy } from "lodash-es";
-import { Qualities, Recorder } from "@bililive-tools/manager";
+import { HuYaQualities, Recorder } from "@bililive-tools/manager";
 
 import { getRoomInfo as getRoomInfoByWeb } from "./huya_api.js";
 import { getRoomInfo as getRoomInfoByMobile } from "./huya_mobile_api.js";
@@ -55,24 +55,33 @@ export async function getStream(
   if (!info.living) {
     throw new Error("It must be called getStream when living");
   }
-
-  let expectStream: StreamProfile;
-  const streamsWithPriority = sortAndFilterStreamsByPriority(info.streams, opts.streamPriorities);
-  if (streamsWithPriority.length > 0) {
-    // 通过优先级来选择对应流
-    expectStream = streamsWithPriority[0];
-  } else {
-    // 通过设置的画质选项来选择对应流
-    const flexedStreams = getValuesFromArrayLikeFlexSpaceBetween(
-      // 接口给的画质列表是按照清晰到模糊的顺序的，这里翻转下
-      info.streams.toReversed(),
-      Qualities.length,
-    );
-    const qn = (
-      Qualities.includes(opts.quality as any) ? opts.quality : "highest"
-    ) as (typeof Qualities)[number];
-    expectStream = flexedStreams[Qualities.indexOf(qn)];
+  if (info.streams.length === 0) {
+    throw new Error(`No stream found in huya ${opts.channelId} room`);
   }
+
+  const qn = (
+    HuYaQualities.includes(opts.quality as any) ? opts.quality : 0
+  ) as (typeof HuYaQualities)[number];
+  let expectStream: StreamProfile = info.streams.find((stream) => stream.bitRate === qn);
+  if (!expectStream) {
+    expectStream = info.streams[0];
+  }
+  // const streamsWithPriority = sortAndFilterStreamsByPriority(info.streams, opts.streamPriorities);
+  // if (streamsWithPriority.length > 0) {
+  //   // 通过优先级来选择对应流
+  //   expectStream = streamsWithPriority[0];
+  // } else {
+  //   // 通过设置的画质选项来选择对应流
+  //   const flexedStreams = getValuesFromArrayLikeFlexSpaceBetween(
+  //     // 接口给的画质列表是按照清晰到模糊的顺序的，这里翻转下
+  //     info.streams.toReversed(),
+  //     Qualities.length,
+  //   );
+  //   const qn = (
+  //     Qualities.includes(opts.quality as any) ? opts.quality : "highest"
+  //   ) as (typeof Qualities)[number];
+  //   expectStream = flexedStreams[Qualities.indexOf(qn)];
+  // }
 
   let expectSource: SourceProfile | null = null;
   const sourcesWithPriority = sortAndFilterSourcesByPriority(info.sources, opts.sourcePriorities);
