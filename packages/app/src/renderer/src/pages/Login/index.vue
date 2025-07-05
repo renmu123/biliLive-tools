e91ee795e9674233396a6db924584af6670ca114
renmu123 <1101022351@qq.com>
2025-07-05 20:22:40 +0800

    update

--- packages/app/src/renderer/src/pages/Login/index.vue
+++ packages/app/src/renderer/src/pages/Login/index.vue
@@ -3,8 +3,6 @@
     <n-space vertical style="text-align: center">
       <n-input v-model:value="api" placeholder="请输入API地址，如http://127.0.0.1:18010" />
       <n-input v-model:value="key" placeholder="密钥" type="password" />
-      <p>接口最低1.6.0如果遇到功能无法访问，请尝试更新至最新版</p>
-      <!-- 有个github链接 -->
       <div style="gap: 10px; display: flex">
         <n-button type="warning" style="flex: 1" @click="test">联通测试</n-button>
         <n-button type="primary" style="flex: 1" @click="login">确认</n-button>
